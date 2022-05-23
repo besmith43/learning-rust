506b4c709c76d752fe29f1fa235b3e03dadd28ff
Blake Smith <besmith43@users.noreply.github.com>
2022-05-23 18:57:58 -0500

    added some vim files to get ignore and created a toy project to play with debugger in vimspector

---
+++ working_with_the_debugger/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    println!("starting the program");
+
+    println!("second line of code");
+
+    println!("third line of code");
+
+    println!("Setting a variable");
+
+    let mut x = 5;
+
+    println!("changing the value");
+
+    x = x + 10;
+
+    println!("the new value of x is {}", x);
+
+    println!("ending the program");
+}
+
+
+
+
+
+
+
