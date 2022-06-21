ee0dee021a93dc191ff2a6d352e7d8cfd44073b8
Blake Smith <besmith43@users.noreply.github.com>
2022-06-20 21:13:18 -0500

    added command to output llvm ir

--- cross_compile/get_assembly/run.sh
+++ cross_compile/get_assembly/run.sh
@@ -10,3 +10,8 @@ cat ./target/debug/deps/get_assembly-*.s
 
 # cat ./target/release/deps/get_assembly-*.s
 
+# get llvm ir
+cargo rustc -- --emit llvm-ir
+
+cat ./target/debug/deps/get_assembly-*.ll
+
